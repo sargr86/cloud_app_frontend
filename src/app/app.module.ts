d56d1cb5d04af63c046184dd523e8360044d3952
Grigor Sargsyan <sargr86@gmail.com>
2019-02-19 09:36:21 +0400

    Added auth/shared modules, register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthService} from "./shared/services/auth.service";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AuthModule
+    ],
+    providers: [AuthService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
