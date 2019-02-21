e4fa084911d68af5a07fd84be0c727a4d794b42c
Grigor Sargsyan <sargr86@gmail.com>
2019-02-21 09:40:50 +0400

    Restored right sidebar, moved profile/home buttons to the sidebar

--- src/app/layout/right-sidebar/right-sidebar.component.ts
+++ src/app/layout/right-sidebar/right-sidebar.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../shared/services/auth.service";
 
 @Component({
-  selector: 'right-sidebar',
-  templateUrl: './right-sidebar.component.html',
-  styleUrls: ['./right-sidebar.component.scss']
+    selector: 'right-sidebar',
+    templateUrl: './right-sidebar.component.html',
+    styleUrls: ['./right-sidebar.component.scss']
 })
 export class RightSidebarComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        public router: Router,
+        public _auth: AuthService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    /**
+     * Navigates to home page or admin dashboard based on role
+     */
+    navigateHome() {
+        this.router.navigate([this._auth.checkRoles('admin') ? 'admin' : '/'])
+    }
+
+    /**
+     * Checks to see if we're on a form editing/saving page
+     * @returns {any}
+     */
+    get formPage() {
+        let routerUrl = this.router.url;
+        return (routerUrl.includes('edit') || routerUrl.includes('add')||routerUrl.includes('profile'));
+    }
 
 }
